c02156c04ad84fc903ff8661f52e3c06ac5774a7
Evgeny Zaletov <zalyotov@mail.ru>
2017-04-10 19:36:54 +0300

    Выполнено 6-е задание

---
+++ lesson_6/route.rb
@@ -0,0 +1,35 @@
+class Route
+  attr_accessor :stations
+
+  def initialize(from, to)
+    self.stations = [from, to]
+    validate!
+  end
+
+  def add_station(station)
+    stations.insert(-2, station)
+  end
+
+  def delete_station(station)
+    stations.delete(station)
+  end
+
+  def show
+    stations.each do |station|
+      puts "#{station.name}"
+    end
+  end
+
+  def valid?
+    validate!
+  rescue StandardError
+    false
+  end
+
+  private
+
+  def validate!
+    raise "Станция не является объектом" unless (stations.first.is_a? Station) || (stations.last.is_a? Station)
+    true
+  end
+end
---
+++ lesson_6/station.rb
@@ -0,0 +1,68 @@
+class Station
+  attr_accessor :trains
+  attr_reader :name
+  # переменная класса
+  @@stations = []
+  NAME_FORMAT = /^[а-я]+$/i
+
+  class << self
+    def all
+      @@stations
+    end
+  end
+
+  def initialize(name)
+    @name = name
+    @trains = []
+    @@stations << self
+    validate!
+  end
+
+  def arrive(train)
+    train.stop if self.trains << train
+  end
+
+  def departure(train)
+    train.go(50) if self.trains.delete(train)
+  end
+
+  # в задании надо "возвращать список поездов на станции 
+  # по типу (см. ниже): кол-во грузовых, пассажирских", 
+  # тоесть вернуть все поезда, отсортированные по типу
+  def train_filter_list
+    result = {cargo: 0, passanger: 0}
+    self.trains.each do |train| 
+      if train.type == "cargo"
+        result[:cargo] += 1
+      else
+        result[:passanger] += 1
+      end
+    end
+    result
+  end
+
+  def train_list
+    result = {cargo: 0, passanger: 0}
+    self.trains.each do |train| 
+      if train.class == CargoTrain
+        result[:cargo] += 1
+      else
+        result[:passanger] += 1
+      end
+    end    
+    result
+  end
+
+  def valid?
+    validate!
+  rescue StandardError
+    false
+  end
+
+  private
+
+  def validate!
+    raise "В названии станции должны быть только русские буквы" if name !~ NAME_FORMAT
+    true
+  end
+end
